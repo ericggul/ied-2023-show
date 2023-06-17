5decc6e4c1f1277d846b57fd0cfa8172ecf09668
ericggul <57379903+ericggul@users.noreply.github.com>
2023-06-17 18:47:21 +0100

    Fix: Prisma Error

--- containers/screen/styles.js
+++ containers/screen/styles.js
@@ -1,8 +1,6 @@
 import { FlexCenterStyle, WholeContainer } from "styles/common";
 import styled from "styled-components";
 
-import { Appear } from "styles/common";
-
 export const Container = styled.div`
   ${WholeContainer}
   ${FlexCenterStyle}
