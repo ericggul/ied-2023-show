80119d54febb18dd8560d5b2be4ec07fd62b4c74
ericggul <57379903+ericggul@users.noreply.github.com>
2023-09-05 15:05:40 +0100

    Feat: V&A Init

--- containers/vna/screen/index.js
+++ containers/vna/screen/index.js
@@ -1,13 +1,13 @@
 import * as S from "./styles";
-import { Fragment, useState, useEffect, useRef, useMemo } from "react";
+import { Fragment, useState, useEffect, useRef, useMemo, use } from "react";
 
 import { PROJECTS, CONNECTIONS, KEYWORDS } from "../data/constant";
 
 //hooks
 import useSocket from "utils/hooks/socket/vna/useSocketScreen";
+import useResize from "utils/hooks/useResize";
 
-//foundations
-import dynamic from "next/dynamic";
+import Image from "next/image";
 
 const URLS = ["/assets/vna/ev1.png", "/assets/vna/ro1.png"];
 
@@ -21,20 +21,75 @@ export default function Screen() {
     setCurrKeyword(keyword);
   }
 
-  console.log(currKeyword.length);
+  const [projectImg, setProjectImg] = useState("ev");
+  const [projectImgTransitioningIdx, setProjectImgTransitioningIdx] = useState(0);
+  const [aiImg, setAIImg] = useState("ev1");
+  const [aiImgTransitioningIdx, setAiImgTransitioningIdx] = useState(0);
+
+  useEffect(() => {
+    setAIImg("ev" + ((currKeyword.length % 3) + 1).toString());
+  }, [currKeyword]);
+
+  ////////////
+  ////project img
+  ////////////
+  useEffect(() => {
+    setProjectImgTransitioningIdx((i) => i + 1);
+  }, [projectImg]);
+
+  ////////////
+  ////ai img
+  ////////////
+
+  useEffect(() => {
+    setAiImgTransitioningIdx((i) => i + 1);
+  }, [aiImg]);
 
   return (
     <S.Container>
       {new Array(4).fill(0).map((_, j) => (
         <S.Row key={j}>
           {new Array(10).fill(0).map((_, i) => (
-            <S.Img key={i}>
-              {(i + j) % 2 === 0 && <img src={"/assets/vna/ev.png"} />}
-              {(i + j) % 2 === 1 && <img src={`/assets/vna/ev${(currKeyword.length % 3) + 1}.png`} />}
-            </S.Img>
+            <Fragment key={i}>
+              {(i + j) % 2 === 0 && <ImageComp i={i} j={j} globalImgURL={projectImg} />}
+              {(i + j) % 2 === 1 && <ImageComp i={i} j={j} globalImgURL={aiImg} />}
+            </Fragment>
           ))}
         </S.Row>
       ))}
     </S.Container>
+  );
+}
+
+function ImageComp({ i, j, globalImgURL }) {
+  const [imgURL, setImgURL] = useState(false);
+  const transitioningIdx = useRef(0);
+  const [transitioning, setTransitioning] = useState(false);
+  const timeoutSeconds = useMemo(() => (i + j) * 50, [i, j]);
+
+  const [windowWidth, windowHeight] = useResize();
+
+  useEffect(() => {
+    let timeout1, timeout2;
+
+    transitioningIdx.current++;
+    timeout1 = setTimeout(() => {
+      setTransitioning(true);
+    }, timeoutSeconds);
+    timeout2 = setTimeout(() => {
+      setImgURL(globalImgURL);
+      setTransitioning(false);
+    }, timeoutSeconds + 300);
+
+    return () => {
+      timeout1 && clearTimeout(timeout1);
+      timeout2 && clearTimeout(timeout2);
+    };
+  }, [globalImgURL, i, j, timeoutSeconds]);
+
+  return (
+    <S.Img transitioning={transitioning} transitioningIdx={transitioningIdx.current}>
+      <Image src={`/assets/vna/${imgURL}.png`} width={windowHeight / 4} height={windowHeight / 4} alt="Image" />
+    </S.Img>
   );
 }
--- containers/vna/screen/styles.js
+++ containers/vna/screen/styles.js
@@ -25,6 +25,11 @@ export const Img = styled.div`
   width: 25vh !important;
   height: 25vh;
 
+  // transform: translateY(${({ transitioning }) => (transitioning ? "-100%" : "0")});
+  transform: rotateY(${({ transitioning }) => (transitioning ? "90deg" : "0deg")});
+  // opacity: ${({ transitioning }) => (transitioning ? "0" : "1")};
+  transition: all 0.2s ease-in-out;
+
   img {
     width: 100%;
     height: 100%;
